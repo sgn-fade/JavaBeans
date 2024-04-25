0ca965879f5328eefca17090fcdf21f8b479d86e
fade <dimabreusgo@gmail.com>
2024-04-25 19:03:17 +0300

    added second bean

---
+++ src/TableBean/DataSheetGraphBeanInfo.java
@@ -0,0 +1,25 @@
+package TableBean;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+public class DataSheetGraphBeanInfo extends SimpleBeanInfo {
+    private PropertyDescriptor[] propertyDescriptors;
+    public DataSheetGraphBeanInfo() throws IntrospectionException {
+        try {
+            propertyDescriptors = new PropertyDescriptor[]
+                    {
+                            new PropertyDescriptor("color",DataSheetGraph.class),
+                            new PropertyDescriptor("filled",DataSheetGraph.class),
+                            new PropertyDescriptor("deltaX",DataSheetGraph.class),
+                            new PropertyDescriptor("deltaY",DataSheetGraph.class)
+                    };
+        } catch (IntrospectionException e) {}
+    }
+
+    @Override
+    public PropertyDescriptor[] getPropertyDescriptors() {
+        return propertyDescriptors;
+    }
+}
